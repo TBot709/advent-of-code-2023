5c18df437b7e9ab34a876e2ad7b67ca9da1fd601
Tim Barlett <tim.a.bartlett@gmail.com>
2023-12-14 14:49:37 -0330

    update day00_template file

--- day00_template.py
+++ day00_template.py
@@ -5,33 +5,38 @@ import threading
 from datetime import datetime
 from panic_thread import PanicThread
 
+# initialize panic thread
 panic_thread = PanicThread(
   threading.current_thread(), 
   PanicThread.ONE_GIGABYTE, 
   PanicThread.TEN_SECONDS)
 panic_thread.start()
 
+# debug print method
 DEBUG = False
-# DEBUG = True
+DEBUG = True
 def debug(msg) -> None:
   if DEBUG:
     print(msg)
 start = datetime.now()
 
-# # # # # # # # # #
-file = open('day00_example-input.txt','r')
-# file = open('day00_input.txt','r')
+# get input lines
+puzzleNumber = "00"
+file = open(f"day{puzzleNumber}_example-input.txt",'r')
+# file = open(f"day{puzzleNumber}_input.txt",'r')
 lines = file.readlines()
 lines = list(map(lambda line: line.strip('\n'), lines))
 nRows = len(lines)
 nColumns = len(lines[0])
 file.close()
-debug(f"{nRows}, {nColumns}")
+debug(f"rows: {nRows}, columns: {nColumns}")
 debug(f"{lines}")
+
+# # # # # # PUZZLE SOLUTION START # # # # # #
 
 print(0)
 
-# # # # # # # # # # 
+# # # # # # PUZZLE SOLUTION END # # # # # # # 
 
 print('finished in ', end='')
 print(datetime.now() - start, end='')
