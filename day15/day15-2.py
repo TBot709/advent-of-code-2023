6b28efb16df8413fde87e6f9cfa3d79e3e70989b
Tim Barlett <tim.a.bartlett@gmail.com>
2024-01-21 12:54:02 -0330

    day15-2 complete

--- day15/day15-2.py
+++ day15/day15-2.py
@@ -7,7 +7,7 @@ from panic_thread import PanicThread
 from debug import debug, setDebug
 
 setDebug(False)
-setDebug(True)
+#setDebug(True)
 
 puzzleNumber = "15"
 partNumber = "2"
@@ -23,9 +23,9 @@ panic_thread.start()
 start = datetime.now()
 
 # get input lines
-file = open(f"./day{puzzleNumber}/day{puzzleNumber}_short-input.txt",'r')
+#file = open(f"./day{puzzleNumber}/day{puzzleNumber}_short-input.txt",'r')
 #file = open(f"./day{puzzleNumber}/day{puzzleNumber}_example-input.txt",'r')
-#file = open(f"./day{puzzleNumber}/day{puzzleNumber}_input.txt",'r')
+file = open(f"./day{puzzleNumber}/day{puzzleNumber}_input.txt",'r')
 lines = file.readlines()
 lines = list(map(lambda line: line.strip('\n'), lines))
 nRows = len(lines)
@@ -61,37 +61,35 @@ for step in steps:
     debug(f"{step} {'=' in step} {'-' in step}")
     if "=" in step:
         splitStep = step.split("=")
-        label = "[" + splitStep[0] + "]"
+        label = splitStep[0]
         iBox = sillyHash(splitStep[0])
         sBox = boxes[iBox]
         focalLength = splitStep[1]
-        iExistingIndex = sBox.find(label)
+        iExistingIndex = sBox.find("[" + label + ":")
         if iExistingIndex != -1:
-            iNextComma = sBox.find(",",iExistingIndex)
-            sBox = ((sBox[0:iExistingIndex - 1]) if iExistingIndex > 0 else "") \
-                    + ("," if iExistingIndex > 0 and iNextComma != -1 else "") \
-                    + label + ":" + focalLength \
-                    + ("," if iNextComma != -1 else "") \
-                    + (sBox[iNextComma + 1::] if iNextComma != -1 else "")
+            iNext = sBox.find("[",iExistingIndex + 1)
+            debug(f"\t{iNext}")
+            sBox = sBox[0:iExistingIndex] \
+                    + "[" + label + ":" + focalLength + "]" \
+                    + (sBox[iNext::] if iNext != -1 else "")
         else:
             if len(sBox) > 0:
-                sBox = sBox + ","  + label + ":" + focalLength
+                sBox = sBox + "["  + label + ":" + focalLength + "]"
             else:
-                sBox = label + ":" + focalLength
+                sBox = "[" + label + ":" + focalLength + "]"
         boxes[iBox] = sBox
         debug(f"\tlabel:{label} fl:{focalLength} index:{iExistingIndex} iBox:{iBox} sBox:{sBox}")
     elif "-" in step:
         splitStep = step.split("-")
-        label = "[" + splitStep[0] + "]"
+        label = splitStep[0]
         iBox = sillyHash(splitStep[0])
         sBox = boxes[iBox]
-        iExistingIndex = sBox.find(label)
+        iExistingIndex = sBox.find("[" + label + ":")
         debug(f"\tlabel:{label} index:{iExistingIndex} iBox:{iBox} sBox:{sBox}")
         if iExistingIndex != -1:
-            iNextComma = sBox.find(",",iExistingIndex)
-            sBox = ((sBox[0:iExistingIndex - 1]) if iExistingIndex > 0 else "") \
-                    + ("," if iExistingIndex > 0 and iNextComma != -1 else "") \
-                    + (sBox[iNextComma + 1::] if iNextComma != -1 else "")
+            iNext = sBox.find("[",iExistingIndex + 1)
+            debug(f"\t{iNext}")
+            sBox = sBox[0:iExistingIndex] + (sBox[iNext::] if iNext != -1 else "")
             boxes[iBox] = sBox
     else:
         raise Exception(f"invalid step, {step}")
@@ -102,11 +100,12 @@ debug(f"\n\n{sBoxes(boxes)}")
 sumFocusingPower = 0
 for iBox, box in enumerate(boxes):
     if len(box) > 0:
+        box = box.replace("][",",").replace("[","").replace("]","")
         for iLense, lense in enumerate(box.split(",")):
             debug(f"{iBox} {box} {iLense} {lense} {lense.split(':')}")
             fp = iBox + 1
             fp *= iLense + 1 
-            fp *= int(lense.split(":")[1])
+            fp *= int(lense.split(":")[1][0])
             sumFocusingPower += fp
 
 print(sumFocusingPower)
