9a69a1828bd13262922116bbfcabbfc881784623
Tim Barlett <tim.a.bartlett@gmail.com>
2024-01-29 17:04:58 -0330

    day17 in prog

--- day17/day17-1.py
+++ day17/day17-1.py
@@ -111,13 +111,10 @@ EAST = '>'
 SOUTH = 'v'
 WEST = '<'
 
-
 def getDistanceAndDirection(firstXYTuple, secondXYTuple):
-    # Calculate the relative coordinates
     relativeX = secondXYTuple[0] - firstXYTuple[0]
     relativeY = secondXYTuple[1] - firstXYTuple[1]
 
-    # Calculate the direction
     if relativeX > 0:
         direction = EAST
     elif relativeX < 0:
@@ -127,34 +124,92 @@ def getDistanceAndDirection(firstXYTuple, secondXYTuple):
     else:
         direction = NORTH
 
-    return ((relativeX, relativeY), direction)
+    if direction == EAST or direction == WEST:
+        distance = relativeX
+    else:
+        distance = relativeY
+
+    if distance < 0:
+        distance *= -1
+
+    return (distance, direction)
+
+
+class PathNode:
+    def __init__(self, x, y, prevDirection, prevJumpDist):
+        self.x = x
+        self.y = y
+        self.prevDirection = prevDirection
+        self.prevJumpDist = prevJumpDist
+
+    def __eq__(self, other):
+        return self.x == other.x and \
+                self.y == other.y and \
+                self.prevDirection == other.prevDirection and \
+                self.prevJumpDist == other.prevJumpDist
+
+    def __hash__(self):
+        return hash(str(self.x) + "," +
+                    str(self.y) + "," +
+                    self.prevDirection + "," +
+                    str(self.prevJumpDist))
+
+    def __lt__(self, other):
+        return self.x <= other.x and self.y <= other.y
+
+    def __str__(self):
+        return f"({self.x}, {self.y}, {self.prevDirection}, {self.prevJumpDist})"
 
 
 def dijkstra(adj_list, start):
-    # rtrn = ((0, 0): (distance, prevNode, prevDirection))
-    rtrn = {node: (float('inf'), (-1, -1), NORTH) for node in adj_list}
-    rtrn[start] = (0, start, NORTH)
+    # rtrn = {PathNode: (distance, prevNode)}
+    rtrn = {}
+    for node in adj_list:
+        for x in range(3):
+            rtrn[PathNode(node[0], node[1], NORTH, x + 1)] = (float('inf'), None)
+            rtrn[PathNode(node[0], node[1], EAST, x + 1)] = (float('inf'), None)
+            rtrn[PathNode(node[0], node[1], SOUTH, x + 1)] = (float('inf'), None)
+            rtrn[PathNode(node[0], node[1], WEST, x + 1)] = (float('inf'), None)
 
-    # priority_queue = [(distance, node, prevDirection)]
-    priority_queue = [(0, start, NORTH)]
+    # debug(list(map(lambda n: str(n), rtrn)))
+
+    startNode = PathNode(start[0], start[1], NORTH, 0)
+    rtrn[startNode] = (0, None)
+
+    # priority_queue = [(distance, node)]
+    priority_queue = [(0, startNode)]
 
     while priority_queue:
-        current_distance, current_node, current_prev_direction = heapq.heappop(priority_queue)
+        current_distance, current_node = heapq.heappop(priority_queue)
+
+        # debug(f"{current_distance}, {current_node}")
 
         if current_distance > rtrn[current_node][0]:
             continue
 
-        for neighbor, weight in adj_list[current_node].items():
-            directionToNeighbour = getDistanceAndDirection(current_node, neighbor)[1]
+        for neighbor, weight in adj_list[(current_node.x, current_node.y)].items():
+            distanceAndDirection = \
+                    getDistanceAndDirection(
+                        (current_node.x, current_node.y),
+                        neighbor)
+            # debug(f"\t{neighbor}, {distanceAndDirection}")
             # skip if direction to this neighbour matched previous jump direction
-            if directionToNeighbour == rtrn[current_node][2]:
+            if distanceAndDirection[1] == current_node.prevDirection:
                 continue
 
             distance = current_distance + weight
+            neighborNode = \
+                PathNode(
+                    neighbor[0],
+                    neighbor[1],
+                    distanceAndDirection[1],
+                    distanceAndDirection[0])
+            # debug(f"\t{neighborNode}")
+            neighborDistance = rtrn[neighborNode][0]
             # If shorter path to neighbor is found, update distance and push to queue
-            if distance < rtrn[neighbor][0]:
-                rtrn[neighbor] = (distance, current_node, directionToNeighbour)
-                heapq.heappush(priority_queue, (distance, neighbor, directionToNeighbour))
+            if distance < neighborDistance:
+                rtrn[neighborNode] = (distance, current_node)
+                heapq.heappush(priority_queue, (distance, neighborNode))
     return rtrn
 
 
@@ -162,16 +217,23 @@ def sDijkstraReturn(d):
     if not d:
         return "The dictionary is empty."
 
-    min_x = min(x for x, y in d.keys())
-    max_x = max(x for x, y in d.keys())
-    min_y = min(y for x, y in d.keys())
-    max_y = max(y for x, y in d.keys())
+    min_x = min(n.x for n in d.keys())
+    max_x = max(n.x for n in d.keys())
+    min_y = min(n.y for n in d.keys())
+    max_y = max(n.y for n in d.keys())
 
     s = "\n"
     for y in range(min_y, max_y + 1):
         for x in range(min_x, max_x + 1):
-            # s += f"[({x}, {y}), " + str(d.get((x, y), '#')) + '] '
-            s += "[" + str(d.get((x, y), '#')) + ']'
+            s += "["
+            for direction in [NORTH, EAST, SOUTH, WEST]:
+                for dist in range(1, 4):
+                    node = PathNode(x, y, direction, dist)
+                    shortestPathDist = d[node]
+                    if shortestPathDist == float('inf'):
+                        shortestPathDist = -1
+                    s += f"{direction}{dist}:{shortestPathDist},"
+            s += "] "
         s += '\n'
     return s
 
@@ -180,21 +242,36 @@ def sDijkstraReturn_pathOnly(d):
     if not d:
         return "The dictionary is empty."
 
-    min_x = min(x for x, y in d.keys())
-    max_x = max(x for x, y in d.keys())
-    min_y = min(y for x, y in d.keys())
-    max_y = max(y for x, y in d.keys())
+    min_x = min(n.x for n in d.keys())
+    max_x = max(n.x for n in d.keys())
+    min_y = min(n.y for n in d.keys())
+    max_y = max(n.y for n in d.keys())
 
-    path = [((max_x, max_y), d.get((max_x, max_y)))]
-    while path[len(path) - 1][0] != (0, 0):
-        nextCell = path[len(path) - 1][1][1]
-        path.append((nextCell, d.get(nextCell)))
+    def shortestAtCoords(coords, d):
+        shortestNode = None
+        shortest = float('inf')
+        for direction in [NORTH, EAST, SOUTH, WEST]:
+            for dist in range(1, 4):
+                node = PathNode(coords[0], coords[1], direction, dist)
+                if d[node][0] < shortest:
+                    shortestNode = node
+                    shortest = d[node][0]
+        return shortestNode
+
+    finalNode = shortestAtCoords((max_x, max_y), d)
+
+    path = [finalNode]
+    while d[path[-1]][1] is not None:
+        path.append(d[path[-1]][1])
+
+    debug(list(map(lambda node: str(node), path)))
 
     s = "\n"
     for y in range(min_y, max_y + 1):
         for x in range(min_x, max_x + 1):
-            if any(map(lambda c: c[0][0] == x and c[0][1] == y, path)):
-                s += str(d.get((x, y), '#')[2])
+            pathNode = next((node for node in path if node.x == x and node.y == y), None)
+            if pathNode is not None:
+                s += pathNode.prevDirection
             else:
                 s += str(grid[y][x])
         s += '\n'
@@ -205,12 +282,25 @@ d = dijkstra(adjMatrix, (0, 0))
 
 # debug(adjMatrix)
 # debug(d)
-debug(sDijkstraReturn(d))
+# debug(list(map(lambda kv: str(kv[0]) + ":" + str(kv[1]) if kv[1] < 9999 else "", d.items())))
+# debug(list(map(lambda kv: str(kv[0]) + ":" + str(kv[1][0]), d.items())))
+# debug(sDijkstraReturn(d))
 debug(sDijkstraReturn_pathOnly(d))
 
-minHeatLoss = d[(nColumns - 1, nRows - 1)][0]
+minDist = float('inf')
+endX = nColumns - 1
+endY = nRows - 1
+for x in range(3):
+    dN = d[PathNode(endX, endY, NORTH, x + 1)][0]
+    dE = d[PathNode(endX, endY, EAST, x + 1)][0]
+    dS = d[PathNode(endX, endY, SOUTH, x + 1)][0]
+    dW = d[PathNode(endX, endY, WEST, x + 1)][0]
+    minDist = dN if dN < minDist else minDist
+    minDist = dE if dE < minDist else minDist
+    minDist = dS if dS < minDist else minDist
+    minDist = dW if dW < minDist else minDist
 
-print(minHeatLoss)
+print(minDist)
 
 # # # # # # PUZZLE SOLUTION END # # # # # # #
 
