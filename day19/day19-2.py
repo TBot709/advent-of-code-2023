897fd6406f8aa94626999f2dd3a9de907f101186
Tim Barlett <tim.a.bartlett@gmail.com>
2024-02-05 23:30:22 -0330

    day19, inaccurate part 2 solution

--- day19/day19-2.py
+++ day19/day19-2.py
@@ -54,7 +54,11 @@ class Operator(Enum):
 class PartRanges:
     def __init__(self, nextWorkflow, ranges: {str: (int, int)}):
         self.nextWorkflow = nextWorkflow
-        self.ranges
+        self.ranges = ranges
+
+    def __str__(self):
+        return f"{self.nextWorkflow}: {self.ranges}"
+
 
 class Rule:
     def __init__(self, op: Operator, cat: str, value: int, destination: str):
@@ -70,22 +74,27 @@ class Rule:
             case Operator.LT:
                 return part[self.cat] < self.value
         return False
-    
+
     def getSplit(self, partRanges) -> (PartRanges, PartRanges):
+        debug(f"getSplits for {self} on {partRanges}")
         contRange = partRanges
-        splitRange = partRanges
-        match self.op:
-            case Operator.GT:
-                contRange.ranges[self.cat][1] == self.value
-                splitRange.ranges[self.cat][0] == self.value + 1
-            case Operator.LT:
-                contRange.ranges[self.cat][0] == self.value + 1
-                splitRange.ranges[self.cat][1] == self.value
+        splitRange = None
+        rangeForCat = contRange.ranges[self.cat]
+        if self.value < rangeForCat[1] and self.value > rangeForCat[0]:
+            splitRange = PartRanges(self.destination, partRanges.ranges.copy())
+            if self.op == Operator.GT:
+                contRange.ranges[self.cat] = (rangeForCat[0], self.value)
+                splitRange.ranges[self.cat] = (self.value + 1, rangeForCat[1])
+            elif self.op == Operator.LT:
+                contRange.ranges[self.cat] = (self.value, rangeForCat[1])
+                splitRange.ranges[self.cat] = (rangeForCat[0], self.value - 1)
+        else:
+            splitRange = None
+        debug(f"\t return \n\t\t({contRange}, \n\t\t{splitRange})")
         return (contRange, splitRange)
-                
 
     def __str__(self):
-        return f"{self.cat}{'<' if self.cat == Operator.LT else '>'}{self.value}:{self.destination}"
+        return f"{self.cat}{'<' if self.op == Operator.LT else '>'}{self.value}:{self.destination}"
 
 
 class Workflow:
@@ -94,13 +103,10 @@ class Workflow:
         self.rules = rules
         self.default = default
 
-    def getSplits(self, partRanges):
-        splits = partRange
-
     def __str__(self):
         s = f"{self.label}" + "{"
         for i, rule in enumerate(self.rules):
-            s += f"{rule.cat}{'<' if rule.cat == Operator.LT else '>'}{rule.value}:{rule.destination},"
+            s += f"{rule},"
         s += f"{self.default}" + "}"
         return s
 
@@ -116,34 +122,64 @@ while lines[iLines] != "":
     parts = partsSection.split(',')
     rules = []
     for part in parts[0:-1]:
+        debug(f"{part} {part.split(':')}")
         s = part.split(':')
         if '>' in s[0]:
             ss = s[0].split('>')
+            debug(f"GT {Rule(Operator.GT, ss[0], int(ss[1]), s[1])}")
             rules.append(Rule(Operator.GT, ss[0], int(ss[1]), s[1]))
         elif '<' in s[0]:
             ss = s[0].split('<')
+            debug(f"LT {Rule(Operator.LT, ss[0], int(ss[1]), s[1])}")
             rules.append(Rule(Operator.LT, ss[0], int(ss[1]), s[1]))
     workflows[label] = Workflow(label, rules, parts[-1])
     iLines += 1
 debug(list(map(lambda wf: f"{wf}", workflows.values())))
 
-partRanges[0] = 
-    PartRange('in', 
-            {'x': (1, 4000), 
-                'm': (1, 4000), 
-                'a': (1, 4000), 
-                's': (1, 4000)})
+partRanges = []
+partRanges.append(PartRanges(
+        'in',
+        {
+            'x': (1, 4000),
+            'm': (1, 4000),
+            'a': (1, 4000),
+            's': (1, 4000)
+        }))
 
 acceptedPartRanges = []
 
-for partRange in partRanges:
-    nxt = workflows[].getNext(part)
-    while nxt not in [ACCEPTED, REJECTED]:
-        nxt = workflows[nxt].getNext(part)
-    if nxt == ACCEPTED:
-        ratingSum += sum(part.values())
+while len(partRanges) > 0:
+    partRange = partRanges[0]
+    debug(f"{partRange}")
+    if partRange.nextWorkflow == ACCEPTED:
+        acceptedPartRanges.append(partRange)
+        partRanges.remove(partRange)
+        continue
+    if partRange.nextWorkflow == REJECTED:
+        partRanges.remove(partRange)
+        continue
 
-print(ratingSum)
+    wf = workflows[partRange.nextWorkflow]
+    for rule in wf.rules:
+        partRange, newRange = rule.getSplit(partRange)
+        if newRange is not None:
+            partRanges.append(newRange)
+
+    debug(f"{partRanges[0].nextWorkflow} set to default {wf.default}")
+    # debug(list(map(lambda x: f"{x}", partRanges)))
+    partRanges[0].nextWorkflow = wf.default
+    # debug(list(map(lambda x: f"{x}", partRanges)))
+
+sumOfPossibleCombos = 0
+
+for ranges in acceptedPartRanges:
+    productOfDiffs = 1
+    debug(f"{ranges}")
+    for r in ranges.ranges.values():
+        productOfDiffs *= r[1] - r[0]
+    sumOfPossibleCombos += productOfDiffs
+
+print(sumOfPossibleCombos)
 
 # # # # # # PUZZLE SOLUTION END # # # # # # #
 
