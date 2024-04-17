abbb3caba7b44839a0ad1a2130b79acc58d29a36
Tim Barlett <tim.a.bartlett@gmail.com>
2024-04-16 22:33:39 -0230

    day 23 in prog

--- day23/day23-1.py
+++ day23/day23-1.py
@@ -45,36 +45,196 @@ for line in lines:
         row.append(c)
     grid.append(row)
 
+
 def strGrid():
-    s += "\n"
+    s = "\n"
     for row in grid:
         for c in row:
             s += c
         s += "\n"
     return s
 
+
 debug(strGrid())
 
+WALL = '#'
+PATH = '.'
 SLOPE_N = '^'
 SLOPE_W = '<'
 SLOPE_E = '>'
 SLOPE_S = 'v'
+NORTH = 'N'
+WEST = 'W'
+EAST = 'E'
+SOUTH = 'S'
+
+directions = [NORTH, WEST, EAST, SOUTH]
+dirTransitions = [(0, -1), (-1, 0), (1, 0), (0, 1)]  # N W E S
+slopes = [SLOPE_N, SLOPE_W, SLOPE_E, SLOPE_S]
+
+
+def getTranslation(coord: (int, int), direction: str):
+    dtIndex = 0
+    if direction == NORTH:
+        dtIndex = 0
+    elif direction == WEST:
+        dtIndex = 1
+    elif direction == EAST:
+        dtIndex = 2
+    elif direction == SOUTH:
+        dtIndex = 3
+    return (coord[0] + dirTransitions[dtIndex][0],
+            coord[1] + dirTransitions[dtIndex][1])
+
+
+def getSlopeTranslation(coord: (int, int), slope: str):
+    if slope == SLOPE_N:
+        return getTranslation(coord, NORTH)
+    elif slope == SLOPE_W:
+        return getTranslation(coord, WEST)
+    elif slope == SLOPE_E:
+        return getTranslation(coord, EAST)
+    elif slope == SLOPE_S:
+        return getTranslation(coord, SOUTH)
+
+
+def isCharAtCoord(char: str, coord: (int, int)):
+    if (coord[0] < 0 or coord[0] >= nColumns or
+            coord[1] < 0 or coord[1] >= nRows):
+        return False
+    return grid[coord[1]][coord[0]] == char
+
+
+def isSlopeAtCoord(coord: (int, int)):
+    if (coord[0] < 0 or coord[0] >= nColumns or
+            coord[1] < 0 or coord[1] >= nRows):
+        return False
+    return grid[coord[1]][coord[0]] in slopes
+
+
+def getCharAtCoord(coord: (int, int)):
+    return grid[coord[1]][coord[0]]
+
 
 class Node:
-    def __init__(self, coord, srcSlope):
+    def __init__(self, coord):
         self.coord = coord
-        self.srcSlope = srcSlope
-        self.explored 
 
-endNode = Node((nColumns - 1, nRows), None)
+        self.entrySlopeCoords = {}
+        t = getTranslation(self.coord, NORTH)
+        if isCharAtCoord(SLOPE_S, t):
+            self.entrySlopeCoords[NORTH] = t
+        t = getTranslation(self.coord, WEST)
+        if isCharAtCoord(SLOPE_E, t):
+            self.entrySlopeCoords[WEST] = t
+        t = getTranslation(self.coord, EAST)
+        if isCharAtCoord(SLOPE_W, t):
+            self.entrySlopeCoords[EAST] = t
+        t = getTranslation(self.coord, SOUTH)
+        if isCharAtCoord(SLOPE_N, t):
+            self.entrySlopeCoords[SOUTH] = t
 
-nodes = [Node((1, 0), None))
-currentNode = (1, 0)
-c: = (1, 1)
-while grid[c[1]][c[0]] not in [SLOPE_N, SLOPE_W, SLOPE_E, SLOPE_S]:
+        self.exitSlopeCoords = {}
+        t = getTranslation(self.coord, NORTH)
+        if isCharAtCoord(SLOPE_N, t):
+            self.exitSlopeCoords[NORTH] = t
+        t = getTranslation(self.coord, WEST)
+        if isCharAtCoord(SLOPE_W, t):
+            self.exitSlopeCoords[WEST] = t
+        t = getTranslation(self.coord, EAST)
+        if isCharAtCoord(SLOPE_E, t):
+            self.exitSlopeCoords[EAST] = t
+        t = getTranslation(self.coord, SOUTH)
+        if isCharAtCoord(SLOPE_S, t):
+            self.exitSlopeCoords[SOUTH] = t
+
+    def __str__(self):
+        return f"{self.coord}, {self.entrySlopeCoords}, {self.exitSlopeCoords}"
+
+    def __eq__(self, other):
+        if not isinstance(other, Node):
+            return False
+        return self.coord[0] == other.coord[0] and self.coord[1] == other.coord[1]
 
 
-print(0)
+endNodeCoord = (nColumns - 2, nRows - 1)
+debug(f"{endNodeCoord}")
+# endNode = Node(endNodeCoord)
+startNode = Node((1, 0))
+startNode.exitSlopeCoords[SOUTH] = (1, 0)
+
+nodes = [startNode]
+
+adjacencies = {}
+adjacencies[startNode.coord] = {}  # no paths to start
+
+for departNode in nodes:
+    debug(f"{departNode}")
+    for slopeD in directions:
+        debug(f"\t{slopeD}")
+        c = ()
+        slope = ()
+        if slopeD in departNode.exitSlopeCoords:
+            slope = departNode.exitSlopeCoords[slopeD]
+            c = getTranslation(slope, slopeD)
+        else:
+            continue
+
+        stepCount = 1
+        prevC = slope
+        isSlopeReached = False
+        while not isSlopeReached:
+            nextDir = ""
+            for nxtD in directions:
+                nxt = getTranslation(c, nxtD)
+                debug(f"\t\tcurrent:{c} {nxtD} next:{nxt} prev:{prevC} steps:{stepCount}")
+                if prevC is not None and nxt == prevC:
+                    continue
+
+                if nxt == endNodeCoord:
+                    stepCount += 1
+                    nodes.append(Node(endNodeCoord))
+                    adjacencies[endNodeCoord] = {departNode.coord: stepCount}
+                    debug(str(nodes[-1]))
+                    isSlopeReached = True  # not a slope, but we want exit
+
+                if isCharAtCoord(PATH, nxt):
+                    prevC = tuple(c)
+                    c = tuple(nxt)
+                    stepCount += 1
+                elif isSlopeAtCoord(nxt):
+                    prevC = tuple(nxt)
+                    c = getSlopeTranslation(nxt, getCharAtCoord(nxt))
+                    stepCount += 2
+                    nodes.append(Node(tuple(c)))
+                    if c not in adjacencies:
+                        adjacencies[tuple(c)] = {}
+                    adjacencies[tuple(c)][departNode.coord] = stepCount
+                    debug(str(nodes[-1]))
+                    isSlopeReached = True
+                    break
+
+for node in nodes:
+    grid[node.coord[1]][node.coord[0]] = 'N'
+
+debug(strGrid())
+
+debug(adjacencies)
+
+maxPath = 0
+currentNodeCoord = endNodeCoord
+while currentNodeCoord != startNode.coord:
+    maxDist = 0
+    maxDistCoord = ()
+    for coord, distance in adjacencies[currentNodeCoord].items():
+        if distance > maxDist:
+            maxDist = distance
+            maxDistCoord = coord
+    maxPath += maxDist
+    debug(f"current: {currentNodeCoord}, maxDist: {maxDist}, maxDistCoord: {maxDistCoord}")
+    currentNodeCoord = maxDistCoord
+
+print(maxPath)
 
 # # # # # # PUZZLE SOLUTION END # # # # # # #
 
