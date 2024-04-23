ec6eb011c0f584fa0f8be9ab6def73058e8b7a29
Tim Barlett <tim.a.bartlett@gmail.com>
2024-04-23 17:23:55 -0230

    day 24 part 1 complete, part 2 in prog

---
+++ day24/day24-2.py
@@ -0,0 +1,139 @@
+#!/usr/bin/python
+
+import threading
+# import resource
+from datetime import datetime
+from panic_thread import PanicThread
+from debug import debug, setDebug
+import math
+
+setDebug(False)
+setDebug(True)
+debug("debug is on")
+
+puzzleNumber = "24"
+partNumber = "2"
+
+# initialize panic thread
+panic_thread = PanicThread(
+  threading.current_thread(),
+  PanicThread.ONE_GIGABYTE,
+  PanicThread.TEN_SECONDS)
+panic_thread.start()
+
+# start now, include file open in running time
+start = datetime.now()
+
+# get input lines
+# file = open(f"./day{puzzleNumber}/day{puzzleNumber}_example-input.txt", 'r')
+file = open(f"./day{puzzleNumber}/day{puzzleNumber}_input.txt",'r')
+lines = file.readlines()
+lines = list(map(lambda line: line.strip('\n'), lines))
+nRows = len(lines)
+nColumns = len(lines[0])
+file.close()
+# debug(f"rows: {nRows}, columns: {nColumns}")
+# debug(f"{lines}")
+
+print(f"# # # # # day{puzzleNumber}-{partNumber} # # # # #")
+
+# # # # # # PUZZLE SOLUTION START # # # # # #
+
+
+class Coord:
+    def __init__(self, x, y, z):
+        self.x = float(x)
+        self.y = float(y)
+        self.z = float(z)
+
+    def __str__(self):
+        return f"{self.x}, {self.y}, {self.z}"
+
+
+class Vector(Coord):
+    pass
+
+
+class Hailstone:
+    def __init__(self, coord: Coord, velocity: Vector):
+        self.coord = coord
+        self.velocity = velocity
+        self.slope2d = velocity.x/velocity.y
+        self.slopeX = math.sqrt(velocity.z * velocity.z + velocity.y * velocity.y)/velocity.x
+        self.slopeY = math.sqrt(velocity.z * velocity.z + velocity.x * velocity.x)/velocity.y
+        self.slopeZ = math.sqrt(velocity.y * velocity.y + velocity.x * velocity.x)/velocity.z
+        self.yIntercept = coord.y - self.slope2d * coord.x
+
+    def __str__(self):
+        return f"{self.coord} @ {self.velocity}, slope2d: {self.slope2d}, slopeX: {self.slopeX}, slopeY: {self.slopeY}, slopeZ: {self.slopeZ}, yInt: {self.yIntercept}"
+
+    # eq of motion x(t) = x(0) + vx * t, so t = (x(t) - x(0))/vx
+    def isPointInFuture(self, point: Coord):
+        if point is None:
+            return False
+        t = (point.x - self.coord.x)/self.velocity.x
+        return t > 0
+
+
+# a Plane can be defined that any two hailstone trajectories lie on
+class Plane:
+
+    # given parametetric form of trajectory lines: 
+    #  (x - x0)/vx = (y - y0)/vy = (z - z0)/vz
+    # 
+    def __init__(self, hsA, hsB):
+        self.normal = 
+
+
+# https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection
+# given hailstoneA: y = ax + c, hailstoneB: y = bx + d
+# intersect point: p = ((d - c)/(a - b), a*((d - c)/(a - b)) + c)
+# slopeDiff = (a - b), intersectDiff = (d - c)
+def getIntersectionPoint(hailstoneA, hailstoneB) -> Coord:
+    slopeDiff = hailstoneA.slope2d - hailstoneB.slope2d
+    if slopeDiff == 0:
+        return None
+    intersectDiff = hailstoneB.yIntercept - hailstoneA.yIntercept
+    intersectOverSlopeDiff = intersectDiff / slopeDiff
+    return Coord(
+            intersectOverSlopeDiff,
+            hailstoneA.slope2d*intersectOverSlopeDiff + hailstoneA.yIntercept,
+            0)
+
+
+PRECISION = 1e-5
+# PRECISION = 1e-10
+
+
+def areParallel3d(hsA, hsB):
+    return (
+            math.isclose(hsA.slopeX, hsB.slopeX, rel_tol=PRECISION)  and
+            math.isclose(hsA.slopeY, hsB.slopeY, rel_tol=PRECISION) and
+            math.isclose(hsA.slopeZ, hsB.slopeZ, rel_tol=PRECISION))
+
+
+hailstones = []
+for line in lines:
+    splitLine = line.split('@')
+    splitCoord = splitLine[0].split(',')
+    splitVelocity = splitLine[1].split(',')
+    hailstone = Hailstone(
+            Coord(splitCoord[0], splitCoord[1], splitCoord[2]),
+            Vector(splitVelocity[0], splitVelocity[1], splitVelocity[2]))
+    # debug(hailstone)
+    hailstones.append(hailstone)
+
+for i, hsA in enumerate(hailstones):
+    for hsB in hailstones[(i + 1):]:
+        # debug(f"\n{hsA}\n{hsB}\n\tareParallel = {areParallel3d(hsA, hsB)}")
+        if areParallel3d(hsA, hsB):
+            debug(f"PARALLEL!\n{hsA}\n{hsB}")
+
+print(0)
+
+# # # # # # PUZZLE SOLUTION END # # # # # # #
+
+print(f"finished in {datetime.now() - start}")
+# peakMemory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+# print(f"peak memory: {peakMemory}")
+panic_thread.end()
